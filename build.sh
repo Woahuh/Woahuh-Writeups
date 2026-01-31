3cd27f6f6d3acdfb32f76265f79853f58f959a79
Woahuh <tk.ankhai2024@gmail.com>
2026-01-31 19:27:14 +0700

    add build script

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# set up an exit on error
+set -o errexit
+
+# Save the initial directory
+ORIGINAL_DIR="$PWD"
+
+# Install specific version of Hugo
+HUGO_VERSION="0.145.0"  # Change this to your required version
+echo "Installing Hugo ${HUGO_VERSION}..."
+
+# Create directory for Hugo download and installation
+mkdir -p "${HOME}/bin"
+mkdir -p /tmp/hugo
+cd /tmp/hugo
+
+# Download and install specific Hugo version
+wget -q https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+tar -xzf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+
+# Move Hugo to a directory you have permission for
+mv hugo "${HOME}/bin/"
+
+# Add the bin directory to PATH
+export PATH="${HOME}/bin:${PATH}"
+
+# Verify installation
+hugo version
+
+# Return to project directory
+cd "$ORIGINAL_DIR"
+
+# Now you can add your Hugo build commands here
+hugo
+echo "Build successfully"
